4d85123184b37fa72f43b44b2c3711c77bdd1d49
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-08-04 23:21:23 +0530

    17518

--- products/views.py
+++ products/views.py
@@ -24,3 +24,30 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class HardCodeMenuView(APIView):
+    """
+    An API view to reterivew a hard code list menu items.
+    This views not interact with databses.
+    """
+    def get(self,request.format=None):
+        """
+        Returns a hardcoded list menu items.
+        """
+        menu_data = [
+            {
+            'name':'Paneer Tikka Masala',
+            'description':'Cubes of paneer (Indian Chese)coocked in rich creamy tomato gravy.',
+            'price':'9.75'
+            },
+            {
+            'name':'Chicken Biryani',
+            'description':'Aromatic basmati rice  coocked with tender chicken pieces and exotic spices.',
+            'price':'3.00'
+            },
+            {
+                'name':'Gulab Jamun',
+                'description':'Deep fried milk solids soaked in a sweet rose flaoured syrup. ',
+                'price':'5.00'
+            }
+        ]
+        return Response(menu_data,status=status.HTTP_200_OK)
