b28aa64e70c2969782702ae8603f42f3a475109a
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-08-05 00:38:36 +0530

    17519

--- account/models.py
+++ account/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class UserProfile(models.Model):
+    """
+    Extend the build-in django User model with additionals field
+    Use OneToOneField to link directly to a User Instance
+    """
+    user  = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    phone_number = models.CharField(max_length=20, blank=True,null=True)
+    bio = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return self.user.username
+@receiver(post_save,sender=User)
+def create_or_update_user_profile(sender,instance,created,**kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        UserProfile.objects.get_or_create(user=instance)
