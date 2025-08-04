2da9b9a454bc7d7d50fc70c2c0c0fab0c8bcf6af
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-08-04 22:39:25 +0530

    17518

--- home/models.py
+++ home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    """
+    A simple model to represent a restaurant
+    This will be used to group menu items.
+    """
+    name =  models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class MeniItem(models.Model):
+    """
+    The model for single menu item (dish)
+    Its includes the name, description , price and link to its restaurant
+    """
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='menu_items') 
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    
