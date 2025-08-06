756a672ae9b43d773a650f5f1277d5b441a9919c
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-08-06 15:49:19 +0530

    Auto-save home/views.py

--- home/views.py
+++ home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+import requests
+
+def home_view(request):
+    menu_items=[]
+    try:
+        api_url = "http://your-api-menu-endpoint/menu/"
+        response = request.get(api_url)
+        response.raise_raise_for_status()
+        menu_items = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error Fetching menu:{e}")
+    
+    context = {
+        'menu_items' :menu_items
+    }
+    return render(request, 'menu.html',context)
