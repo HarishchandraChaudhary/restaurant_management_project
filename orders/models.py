ef2cfabf6d30c2e4e533989bc8a7fe3965f3bcc7
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-08-07 23:17:59 +0530

    17521

--- orders/models.py
+++ orders/models.py
@@ -1,22 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MenuItem(models.Model):
+    name  = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6,decimal_places = 2)
+    # The __str__ methos is important for making objects human readable in the admin.
+    def __str__(self):
+        return self.name
+
+
 class Order(models.Model):
-  order_id = models.AutoField(primary_key=True)
-  customer_name = models.CharField(max_length=100)
-  note = models.TextField(blank=True,null=True)
-  total_order_amount = models.FloatField(default=0.0)
-  is_paid = models = BooleanField(default=False)
-  created_at = models.DateTimeField(auto_now_add=True)
+    STATUS_CHOICES= (
+        ('pending','Pending'),
+        ('processing','Processing'),
+        ('shipped','Shipped'),
+        ('delivered','Delivered'),
+        ('canceled','Canceled'),
+    )
+
+    customer = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    order_items = models.ManyToManyField(MenuItem,through='OrderItem')
+
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2,default=0.00)
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES,default='pending')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order # {self.id} for {self.customer.username}"
 
 
-  def __str__(self):
-    return f"Order #{self.order_id} by {self.customer_name}"
+
 class OrderItem(models.Model):
-  order = models.CharField(Order,on_delete=models.CASCADE, related_name="items")
-  item_name = models.CharField(max_length=100)
-  qnty = models.IntegerField(default=1)
-  created_at = models.DateTimeField(auto_now_add=True)
-  
-  def __str__(self):
-    return f"{self.qynty} * {self.item_name} for Order #{self.order.order_name}"
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+
+    def __str__(self):
+        return f"{self.quantity} of {self.menu_item.name} for Order #{self.order.id}"
