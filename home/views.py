b924063e429d97aa8c1a82e5b2bff1e13d869e17
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-08-07 22:46:45 +0530

    17520

--- home/views.py
+++ home/views.py
@@ -1,19 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-import requests
-
-def home_view(request):
-    menu_items=[]
-    try:
-        api_url = "http://localhost:8000/api/menu/"
-        response = request.get(api_url,timeout=5)
-        response.raise_raise_for_status()
-        menu_items = response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error Fetching menu:{e}")
-    
-    context = {
-        'menu_items' :menu_items
-    }
-    return render(request, 'menu.html',context)
+def home_page_view(request):
+    return render(request,'home/home.html',{})
