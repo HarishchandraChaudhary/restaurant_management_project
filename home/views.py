e2ff002fd889b18f59e765549acebe3017b87b9c
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-08-12 00:47:51 +0530

    30622

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home_page_view(request):
-    return render(request,'home/index.html',{})
+    return render(request,'home/index.html')
+
+
+def about(request):
+    return render(request,'home/about.html')
