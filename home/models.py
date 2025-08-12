8d2263b12358ac2752c0095195196a313d5e90a0
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-08-12 20:05:40 +0530

    30621

--- home/models.py
+++ home/models.py
@@ -6,10 +6,35 @@ class Restaurant(models.Model):
     A simple model to represent a restaurant
     This will be used to group menu items.
     """
-    name =  models.CharField(max_length=200)
+    name = CharField(max_length=200,default="M Restaurant")
+    tagline = models.CharField(max_length=300,blank=True)
+    phone = models.CharField(max_length=20,blank=True)
+    email = models.EmailField(blank=True)
+    address = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    logo = models.ImageField(upload_to='restaurant/',blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Restaurant Information"
+        verbose_name_plural = "Restaurant Information"
+
 
     def __str__(self):
         return self.name
+
+@classmethod
+def get_restaurant_info(cls):
+    """
+    Get the active restaurant info for crate default"""
+    restaurant = cls.objects.filter(is_active=True).first()
+    if not restaurant:
+        restaurant= cls.objects.create(
+            name="My Restaurant",
+            tagline="Welcome to our restaurant"
+        )
+    return restaurant
 
 
 class MenuItem(models.Model):
