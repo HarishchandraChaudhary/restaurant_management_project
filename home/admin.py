6fe32a1736d6808515c70a330ee0f3289ce9cdd3
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-08-12 20:50:26 +0530

    30621

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Restaurant
 
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['name','tagline','is_active','created_at']
+    list_filter = ['is_active','created_at']
+    search_fields = ['name','tagline']
+    fields = ['name','tagline','phone','email','address','logo','is_active']
+    
 # Register your models here.
