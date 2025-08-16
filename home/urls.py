c466f95d1e4a4e40dd8842f3cc3c669bed148ab0
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-08-16 19:42:09 +0530

    33821

--- home/urls.py
+++ home/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('about/',views.about, name='about'),
     path('menu/',views.menu,name='menu'),
     path('contact/',views.contact, name='contact')
+    path('',views.index, name='index'),
 ]
