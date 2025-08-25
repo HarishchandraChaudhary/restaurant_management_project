cd073a304e2fe4147741cf7a3c92061884eeabf3
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-08-25 19:42:29 +0530

    37111

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-urlpatterns = [
+urlpatterns = 
+[
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
     # path('menu_api/',include('menu_api.urls')),
