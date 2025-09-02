e16d43306c2a959144ee6d8fc998bc3eb2918433
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-09-02 17:51:05 +0530

    40090

--- orders/models.py
+++ orders/models.py
@@ -43,3 +43,15 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.menu_item.name} for Order #{self.order.id}"
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to = 'menu_images')
+
+
+    def __str__(self):
+        return self.name
+        
