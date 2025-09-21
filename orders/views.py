3fd936bc95ae60e2c1f199f5cdf0a097941b80ef
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-09-21 22:02:55 +0530

    44225

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Restaurant, MenuItem
 
-
+def homepage(request):
+    try:
+        resutaurant = Restaurant.objects.prefetch_related('opening_hours').get(pk=1)
+    except Restaurant.DoesNotExist:
+        resutaurant = None
+    query = request.GET.get('q','')
+    if query:
+        menu_items = MenuItem.objects.filter(name__icontains=query)
+    else:
+        menu_items = MenuItem.objects.all()
+    return render(request,'homepage.html',context)
