fd54d7eb4d4a2c76819c9d03d9b16e1b77117aea
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-10-02 21:32:35 +0530

    52218

--- orders/models.py
+++ orders/models.py
@@ -68,3 +68,19 @@ class Coupon(models.Model):
     class Meta:
         verbose_name = 'Coupon'
         verbose_name_plural='Coupons'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    description = models.TextField(blank==True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.name
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE, related_name='Category')
