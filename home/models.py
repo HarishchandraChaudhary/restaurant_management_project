a8409c0109713e9112dc9112842a7048ab1e706a
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-10-07 22:51:16 +0530

    53962

--- home/models.py
+++ home/models.py
@@ -24,6 +24,14 @@ class RestaurantInfo(models.Model):
     def __str__(self):
         return self.name
 
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    unit_price = models.DecimalField(max_digits=6,decimal_places =2)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
 
 
 class RestaurantLocation(models.Model):
