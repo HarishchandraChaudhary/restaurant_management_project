29f25143daeeb4b3315a23fbd1a67f013f1e9ef4
Harishchandra Chaudhary <98732153+HarishchandraChaudhary@users.noreply.github.com>
2025-10-07 23:04:01 +0530

    53961

--- orders/views.py
+++ orders/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shorcuts import get_object_or_404
+from .models import Order
+from .serializers import OrderSerializer
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 
-# Create your views here.
-from .models import Restaurant, MenuItem
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().order_by('-order_date')
+    serializer_class = OrderSerializer
 
-def homepage(request):
-    try:
-        resutaurant = Restaurant.objects.prefetch_related('opening_hours').get(pk=1)
-    except Restaurant.DoesNotExist:
-        resutaurant = None
-    query = request.GET.get('q','')
-    if query:
-        menu_items = MenuItem.objects.filter(name__icontains=query)
-    else:
-        menu_items = MenuItem.objects.all()
-    return render(request,'homepage.html',context)
+    @action(detail=True, methods=['post'],url_path='cancel')
+    def cancel_order(self,request,pk=None):
+        order = get_object_or_404(Order,pk=pk)
+
+        if order.status in ['C','L']:
+            return Response(
+                {'detail':f"Order {pk} cannot be cancelled because its stats}"
+                ,status = status.HTTP_400_BAD_REQUEST
+            )
+            order.status = 'C'
+            order.save(update_fields=['status'])
+            serializer = self.get_serializer(order)
+            return Response(serializer.data,status=HTTP_200_OK)
